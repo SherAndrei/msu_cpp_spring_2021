3799aeda6d7b4b5167c58cb523962f90d278e4f1
sherandrei <welldryus@gmail.com>
2021-03-27 01:28:38 +0300

    token try

--- 02/parser.cpp
+++ 02/parser.cpp
@@ -14,14 +14,14 @@ void LeftStrip(std::string_view& sv) {
         sv.remove_prefix(1);
 }
 
-// std::string_view ReadToken(std::string_view& sv) {
-//     LeftStrip(sv);
+std::string_view ReadToken(std::string_view& sv) {
+    LeftStrip(sv);
 
-//     auto pos = sv.find_first_of(" \t\n");
-//     auto result = sv.substr(0, pos);
-//     sv.remove_prefix(pos != sv.npos ? pos : sv.size());
-//     return result;
-// }
+    auto pos = sv.find_first_of(" \t\n");
+    auto result = sv.substr(0, pos);
+    sv.remove_prefix(pos != sv.npos ? pos : sv.size());
+    return result;
+}
 
 }  // namespace
 
@@ -29,15 +29,11 @@ void Parser::run(std::string_view sv) const {
     _startCB();
     uint64_t number = -1;
 
-    do {
-        LeftStrip(sv);
-        auto [ptr, errc] = std::from_chars(sv.begin(), sv.end(), number);
-        if (errc == std::errc() && std::isspace(*ptr)) {
-            _numCB(number);
-        } else if (*ptr != '\0') {
-            _strCB(std::string_view(sv.begin(), ptr - sv.begin()));
-        }
-    } while (!sv.empty());
+    while (!sv.empty()) {
+        auto token = ReadToken(sv);
+        auto [ptr, errc] = std::from_chars(token.begin(), token.end(), number);
 
+        (errc != std::errc() || ptr != token.end()) ? _strCB(token) : _numCB(number);
+    }
     _endCB();
 }
