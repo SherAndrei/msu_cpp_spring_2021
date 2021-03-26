35f275dec344cf5d8ade21816e6eaece8f28a5e5
sherandrei <welldryus@gmail.com>
2021-03-27 02:40:09 +0300

    testUsage

--- 02/test.cpp
+++ 02/test.cpp
@@ -3,7 +3,8 @@
 
 void TestInteger();
 void TestString();
-void TestMixed();
+void TestStartAndEnd();
+void TestUsage();
 
 void TestInteger() {
     {
@@ -71,13 +72,74 @@ void TestString() {
     ASSERT(res == "aa,aa,aa,a11a1a1a1a");
 }
 
-void TestMixed() {
+void TestStartAndEnd() {
+    auto run_parser_4_times = [&] (const Parser& p) {
+        for (auto& str : {"1", "2", "3", "4"}) {
+            p.run(str);
+        }
+    };
+    {
+        Parser pr;
+        size_t counter = 0u;
+        pr.set_start_callback([&]{
+            counter++;
+        });
+        pr.set_end_callback([&]{
+            counter++;
+        });
+        run_parser_4_times(pr);
+        ASSERT(counter == 8u);
+    }
+    {
+        Parser pr;
+        size_t counter = 0u;
+        pr.set_start_callback([&]{
+            counter++;
+        });
+        run_parser_4_times(pr);
+        ASSERT(counter == 4u);
+    }
+    {
+        Parser pr;
+        size_t counter = 0u;
+        pr.set_end_callback([&]{
+            counter++;
+        });
+        run_parser_4_times(pr);
+        ASSERT(counter == 4u);
+    }
+}
 
+void TestUsage() {
+    Parser table_parser;
+    uint64_t sum;
+    std::string result;
+    table_parser.set_start_callback([&] {
+        sum = 0u;
+        result = "Groups:\n";
+    });
+    table_parser.set_number_callback([&] (uint64_t num) {
+        sum += num;
+    });
+    table_parser.set_string_callback([&] (std::string_view sv) {
+        result += std::string(sv) + '\n';
+    });
+    table_parser.set_end_callback([&] {
+        result += "Sum: " + std::to_string(sum) + '\n';
+    });
+    table_parser.run(
+        "BD-11 100\t"
+        "BD-21 350 "
+        "T-Lab-11 1000\n"
+        "Back-end-11 450\t\n"
+        "Golang-11 100\t\n ");
+    ASSERT_EQUAL(result, "Groups:\nBD-11\nBD-21\nT-Lab-11\nBack-end-11\nGolang-11\nSum: 2000\n");
 }
 
 int main() {
     TestRunner tr;
+    RUN_TEST(tr, TestStartAndEnd);
     RUN_TEST(tr, TestInteger);
     RUN_TEST(tr, TestString);
-    RUN_TEST(tr, TestMixed);
+    RUN_TEST(tr, TestUsage);
 }
